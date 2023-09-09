a7f10e46a160f188d754c784394e4c46ed428a03
Himanshu Rawat <87072170+himanshurawat2001@users.noreply.github.com>
2023-09-09 12:28:11 +0530

    Update Dockerfile.py

--- Example/Dockerfile.py
+++ Example/Dockerfile.py
@@ -1,94 +1,106 @@
-from flask import Flask, request, jsonify
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app = Flask(__name__)
+const app = express();
+const port = 3000; // You can change this to your desired port
 
-pantry_data = {}
+app.use(bodyParser.json());
 
-@app.route('/add-item', methods=['POST'])
-def add_item():
-    try:
-        data = request.get_json()
-        pantry_id = data.get('pantry_id')
-        basket_key = data.get('basket_key')
-        value = data.get('value')
-        
-        if pantry_id and basket_key:
-            pantry = pantry_data.setdefault(pantry_id, {})
-            pantry[basket_key] = value
-            return jsonify({"message": "Item added successfully"}), 201
-        else:
-            return jsonify({"message": "Pantry ID and basket key are required"}), 400
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+const pantryData = {};
 
-@app.route('/get-item', methods=['GET'])
-def get_item():
-    try:
-        pantry_id = request.args.get('pantry_id')
-        basket_key = request.args.get('basket_key')
-        
-        if pantry_id and basket_key and pantry_id in pantry_data and basket_key in pantry_data[pantry_id]:
-            value = pantry_data[pantry_id][basket_key]
-            return jsonify({"value": value}), 200
-        else:
-            return jsonify({"message": "Item not found"}), 404
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+app.post('/add-item', (req, res) => {
+    try {
+        const { pantry_id, basket_key, value } = req.body;
 
-@app.route('/list-baskets', methods=['GET'])
-def list_baskets():
-    try:
-        pantry_id = request.args.get('pantry_id')
-        filter_name = request.args.get('filter_name')
-        
-        if pantry_id in pantry_data:
-            pantry = pantry_data[pantry_id]
-            
-            if filter_name:
-                filtered_baskets = {k: v for k, v in pantry.items() if filter_name in k}
-                return jsonify(filtered_baskets), 200
-            else:
-                return jsonify(pantry), 200
-        else:
-            return jsonify({"message": "Pantry not found"}), 404
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+        if (pantry_id && basket_key) {
+            const pantry = pantryData[pantry_id] || {};
+            pantry[basket_key] = value;
+            pantryData[pantry_id] = pantry;
+            res.status(201).json({ message: 'Item added successfully' });
+        } else {
+            res.status(400).json({ message: 'Pantry ID and basket key are required' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
-@app.route('/update-item', methods=['PUT'])
-def update_item():
-    try:
-        data = request.get_json()
-        pantry_id = data.get('pantry_id')
-        basket_key = data.get('basket_key')
-        new_value = data.get('new_value')
-        
-        if pantry_id and basket_key and new_value:
-            if pantry_id in pantry_data and basket_key in pantry_data[pantry_id]:
-                pantry_data[pantry_id][basket_key] = new_value
-                return jsonify({"message": "Item updated successfully"}), 200
-            else:
-                return jsonify({"message": "Item not found"}), 404
-        else:
-            return jsonify({"message": "Pantry ID, basket key, and new value are required"}), 400
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+app.get('/get-item', (req, res) => {
+    try {
+        const { pantry_id, basket_key } = req.query;
 
-@app.route('/delete-item', methods=['DELETE'])
-def delete_item():
-    try:
-        pantry_id = request.args.get('pantry_id')
-        basket_key = request.args.get('basket_key')
-        
-        if pantry_id and basket_key:
-            if pantry_id in pantry_data and basket_key in pantry_data[pantry_id]:
-                del pantry_data[pantry_id][basket_key]
-                return jsonify({"message": "Item deleted successfully"}), 200
-            else:
-                return jsonify({"message": "Item not found"}), 404
-        else:
-            return jsonify({"message": "Pantry ID and basket key are required"}), 400
-    except Exception as e:
-        return jsonify({"message": str(e)}), 500
+        if (pantry_id && basket_key && pantryData[pantry_id] && pantryData[pantry_id][basket_key]) {
+            const value = pantryData[pantry_id][basket_key];
+            res.status(200).json({ value });
+        } else {
+            res.status(404).json({ message: 'Item not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.get('/list-baskets', (req, res) => {
+    try {
+        const { pantry_id, filter_name } = req.query;
+
+        if (pantry_id in pantryData) {
+            const pantry = pantryData[pantry_id];
+
+            if (filter_name) {
+                const filteredBaskets = Object.fromEntries(
+                    Object.entries(pantry).filter(([k]) => k.includes(filter_name))
+                );
+                res.status(200).json(filteredBaskets);
+            } else {
+                res.status(200).json(pantry);
+            }
+        } else {
+            res.status(404).json({ message: 'Pantry not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.put('/update-item', (req, res) => {
+    try {
+        const { pantry_id, basket_key, new_value } = req.body;
+
+        if (pantry_id && basket_key && new_value) {
+            if (pantry_id in pantryData && basket_key in pantryData[pantry_id]) {
+                pantryData[pantry_id][basket_key] = new_value;
+                res.status(200).json({ message: 'Item updated successfully' });
+            } else {
+                res.status(404).json({ message: 'Item not found' });
+            }
+        } else {
+            res.status(400).json({ message: 'Pantry ID, basket key, and new value are required' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.delete('/delete-item', (req, res) => {
+    try {
+        const { pantry_id, basket_key } = req.query;
+
+        if (pantry_id && basket_key) {
+            if (pantry_id in pantryData && basket_key in pantryData[pantry_id]) {
+                delete pantryData[pantry_id][basket_key];
+                res.status(200).json({ message: 'Item deleted successfully' });
+            } else {
+                res.status(404).json({ message: 'Item not found' });
+            }
+        } else {
+            res.status(400).json({ message: 'Pantry ID and basket key are required' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
